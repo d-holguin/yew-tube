04daf86aae6ecaca9350d53c3757aa65a490af95
Daniel <d.holguin@protonmail.com>
2023-04-03 17:22:52 -0700

    Deploy to gh-pages

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -12,50 +12,31 @@ fi
 if [ "$(git rev-parse --abbrev-ref HEAD)" != "gh-pages" ]; then
   # If not, create the gh-pages branch if it doesn't exist.
   if ! git show-ref --verify --quiet refs/heads/gh-pages; then
-    git branch gh-pages
+    if ! git branch gh-pages; then
+      echo "Failed to create gh-pages branch"
+      exit 1
+    fi
   fi
 
   # Checkout the gh-pages branch.
-  git checkout gh-pages
+  if ! git checkout gh-pages; then
+    echo "Failed to checkout gh-pages branch"
+    exit 1
+  fi
 fi
 
-# Remove old build files
-rm -rf dist
 
 # Build the Yew application using Trunk
 trunk build --release
 
-# Move the contents of the dist directory to the root directory
-cp -R dist/* .
+# Remove old build files
+rm -rf docs
 
-# Find the generated file names within the dist directory
-js_file=$(find ./dist -name "yew_tube_converter*.js" -exec basename {} \;)
-wasm_file=$(find ./dist -name "yew_tube_converter*_bg.wasm" -exec basename {} \;)
+# Create the docs directory if it doesn't exist
+mkdir -p docs
 
-# Generate the index.html file with the correct file names
-cat << EOF > index.html
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>YewTube Converter</title>
-    <link rel="icon" href="assets/favicon.svg" type="image/x-icon">
-</head>
-
-<body>
-    <noscript>
-        <strong>Your browser does not support JavaScript or it is disabled. Please enable JavaScript to use this
-            application.</strong>
-    </noscript>
-    <div id="app"></div>
-    <script type="module">
-        import init from './${js_file}';
-        init('./${wasm_file}');
-    </script>
-</body>
-</html>
-EOF
+# Copy the contents of the dist directory to the docs directory
+cp -Rf dist/* docs
 
 #Commit and push changes to the gh-pages branch.
 git add .
