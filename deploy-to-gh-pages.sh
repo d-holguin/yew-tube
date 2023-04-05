cbdd5b4767ddd750e06d1a99f5165b9a4862bf1d
Daniel <d.holguin@protonmail.com>
2023-04-04 18:55:18 -0700

    updating script

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo "This script will deploy to github pages. Are you sure you want to continue? (y/n)"
+echo "This script will deploy to GitHub Pages. Are you sure you want to continue? (y/n)"
 read answer
 
-if [ "$answer" = "${answer#[Yy]}" ]; then 
+if [ "$answer" = "${answer#[Yy]}" ]; then
     echo "Aborted."
     exit 0
 fi
@@ -24,24 +24,32 @@ if [ "$(git rev-parse --abbrev-ref HEAD)" != "gh-pages" ]; then
   fi
 fi
 
+# Backup the current directory, excluding the target directory
+echo "Backing up current directory..."
+backup_dir=".backup_$(date +%Y%m%d%H%M%S)"
+mkdir $backup_dir
+find . -maxdepth 1 -mindepth 1 -type f -exec cp -t $backup_dir {} \;
+find . -maxdepth 1 -mindepth 1 -type d -not -name 'target' -not -name $backup_dir -not -name '.git' -exec cp -a {} $backup_dir/ \;
+echo "Backup created at $backup_dir"
 
-rm -rf docs
-
+# Remove all root files except .git folder
+echo "Cleaning root directory..."
+find . -maxdepth 1 -mindepth 1 -type f -exec rm -f {} \;
+find . -maxdepth 1 -mindepth 1 -type d -not -name '.git' -not -name $backup_dir -exec rm -rf {} \;
+echo "Root directory cleaned"
 
 # Build the Yew application using trunk
 trunk build --release
 
+# Move the contents of the dist folder to the root
+mv dist/* .
+
 # Create the .nojekyll file to disable Jekyll processing
-touch docs/.nojekyll
+touch .nojekyll
 
 # Commit and push changes to the gh-pages branch.
 git add .
 git commit -m "Deploy to gh-pages"
 git push origin gh-pages
-
-# Remove the docs directory
-echo "Removing artifacts..."
-rm -rf docs
-echo "Artifacts removed"
 
 echo "Finished deploying to gh-pages"
