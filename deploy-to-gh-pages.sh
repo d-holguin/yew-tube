097ef7b8ce0b2147f32aaed8dcdb4006cd7914a8
Daniel <d.holguin@protonmail.com>
2023-04-04 19:40:31 -0700

    Deploy to gh-pages

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -8,6 +8,12 @@ if [ "$answer" = "${answer#[Yy]}" ]; then
     exit 0
 fi
 
+# Check if the trunk command is available
+if ! command -v trunk &> /dev/null; then
+    echo "Error: trunk command not found. Please ensure Trunk is installed and available in your PATH."
+    exit 1
+fi
+
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 if [ "$current_branch" != "gh-pages" ]; then
@@ -17,8 +23,16 @@ else
     echo "You are on the gh-pages branch. Proceeding with the deployment..."
 fi
 
+config_file="trunk-gh-pages.toml"
+
+# Check if the trunk-gh-pages.toml file exists
+if [ ! -f "$config_file" ]; then
+    echo "Error: $config_file not found."
+    exit 1
+fi
+
 # Set the TRUNK_CONFIG environment variable to use the gh-pages configuration
-export TRUNK_CONFIG=trunk-gh-pages.toml
+export TRUNK_CONFIG=$config_file
 
 # Build the Yew application using trunk with the release configuration
 trunk build --release
@@ -28,7 +42,9 @@ echo "Committing and pushing changes to gh-pages branch..."
 git add .
 git commit -m "Deploy to gh-pages"
 git push origin gh-pages
+
+# Remove the docs folder
 echo "Removing docs folder..."
-rm -r docs
+rm -rf docs
 
 echo "Finished deploying to gh-pages"
